67a3f9fbc0228cab7f9a52d57763a3922dab7916
Emma Klint <emma@emmaklint.com>
2017-12-07 15:26:51 +0100

    Basic navigation

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,16 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    
+
+    @IBOutlet weak var dayTableView: UITableView!
+    
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        dayTableView.delegate = self
+        dayTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dayTableView.dequeueReusableCell(withIdentifier: "dayCell")
+        cell?.textLabel?.text = items[indexPath.row]
+        return cell!
     }
 
 
